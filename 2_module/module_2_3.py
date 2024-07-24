885c54864036f3e40f2fe4ae3d6a225128789f64
EgorovDm <di.corp@yahoo.com>
2024-07-24 10:13:19 +0300

    2d module 2d tasks

--- 2_module/module_2_3.py
+++ 2_module/module_2_3.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+
+my_list, index =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5], 0
+
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    else:
+        print(my_list[index])
+    index +=1
